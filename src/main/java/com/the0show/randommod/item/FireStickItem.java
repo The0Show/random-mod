9c153725b66ed2bf5cc422815e59ba548c32b425
The0Show <jomst@DESKTOP-9RF5ELP>
2020-09-16 17:31:14 -0400

    added firestick

---
+++ src/main/java/com/the0show/randommod/item/FireStickItem.java
@@ -0,0 +1,131 @@
+
+package com.the0show.randommod.item;
+
+import net.minecraftforge.registries.ObjectHolder;
+import net.minecraftforge.api.distmarker.OnlyIn;
+import net.minecraftforge.api.distmarker.Dist;
+
+import net.minecraft.world.World;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.Direction;
+import net.minecraft.util.ActionResultType;
+import net.minecraft.item.ItemUseContext;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Item;
+import net.minecraft.inventory.EquipmentSlotType;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.entity.ai.attributes.AttributeModifier;
+import net.minecraft.entity.SharedMonsterAttributes;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.block.BlockState;
+
+import java.util.Map;
+import java.util.HashMap;
+
+import com.the0show.randommod.procedures.FireStickRightClickedOnBlockProcedure;
+import com.the0show.randommod.procedures.FireStickLivingEntityIsHitWithToolProcedure;
+import com.the0show.randommod.itemgroup.RandomModTabItemGroup;
+import com.the0show.randommod.RandomModModElements;
+
+import com.google.common.collect.Multimap;
+
+@RandomModModElements.ModElement.Tag
+public class FireStickItem extends RandomModModElements.ModElement {
+	@ObjectHolder("random_mod:fire_stick")
+	public static final Item block = null;
+	public FireStickItem(RandomModModElements instance) {
+		super(instance, 3);
+	}
+
+	@Override
+	public void initElements() {
+		elements.items.add(() -> new ItemToolCustom() {
+			@Override
+			public ActionResultType onItemUse(ItemUseContext context) {
+				ActionResultType retval = super.onItemUse(context);
+				World world = context.getWorld();
+				BlockPos pos = context.getPos();
+				PlayerEntity entity = context.getPlayer();
+				Direction direction = context.getFace();
+				int x = pos.getX();
+				int y = pos.getY();
+				int z = pos.getZ();
+				ItemStack itemstack = context.getItem();
+				{
+					Map<String, Object> $_dependencies = new HashMap<>();
+					$_dependencies.put("x", x);
+					$_dependencies.put("y", y);
+					$_dependencies.put("z", z);
+					$_dependencies.put("world", world);
+					FireStickRightClickedOnBlockProcedure.executeProcedure($_dependencies);
+				}
+				return retval;
+			}
+
+			@Override
+			public boolean hitEntity(ItemStack itemstack, LivingEntity entity, LivingEntity sourceentity) {
+				boolean retval = super.hitEntity(itemstack, entity, sourceentity);
+				double x = entity.getPosX();
+				double y = entity.getPosY();
+				double z = entity.getPosZ();
+				World world = entity.world;
+				{
+					Map<String, Object> $_dependencies = new HashMap<>();
+					$_dependencies.put("entity", entity);
+					FireStickLivingEntityIsHitWithToolProcedure.executeProcedure($_dependencies);
+				}
+				return retval;
+			}
+
+			@Override
+			@OnlyIn(Dist.CLIENT)
+			public boolean hasEffect(ItemStack itemstack) {
+				return true;
+			}
+		}.setRegistryName("fire_stick"));
+	}
+	private static class ItemToolCustom extends Item {
+		protected ItemToolCustom() {
+			super(new Item.Properties().group(RandomModTabItemGroup.tab).maxDamage(100));
+		}
+
+		@Override
+		public Multimap<String, AttributeModifier> getAttributeModifiers(EquipmentSlotType equipmentSlot) {
+			Multimap<String, AttributeModifier> multimap = super.getAttributeModifiers(equipmentSlot);
+			if (equipmentSlot == EquipmentSlotType.MAINHAND) {
+				multimap.put(SharedMonsterAttributes.ATTACK_DAMAGE.getName(),
+						new AttributeModifier(ATTACK_DAMAGE_MODIFIER, "Tool modifier", 2f, AttributeModifier.Operation.ADDITION));
+				multimap.put(SharedMonsterAttributes.ATTACK_SPEED.getName(),
+						new AttributeModifier(ATTACK_SPEED_MODIFIER, "Tool modifier", -3, AttributeModifier.Operation.ADDITION));
+			}
+			return multimap;
+		}
+
+		@Override
+		public boolean canHarvestBlock(BlockState state) {
+			return 0 >= state.getHarvestLevel();
+		}
+
+		@Override
+		public float getDestroySpeed(ItemStack itemstack, BlockState blockstate) {
+			return 2f;
+		}
+
+		@Override
+		public boolean hitEntity(ItemStack stack, LivingEntity target, LivingEntity attacker) {
+			stack.damageItem(1, attacker, i -> i.sendBreakAnimation(EquipmentSlotType.MAINHAND));
+			return true;
+		}
+
+		@Override
+		public boolean onBlockDestroyed(ItemStack stack, World worldIn, BlockState state, BlockPos pos, LivingEntity entityLiving) {
+			stack.damageItem(1, entityLiving, i -> i.sendBreakAnimation(EquipmentSlotType.MAINHAND));
+			return true;
+		}
+
+		@Override
+		public int getItemEnchantability() {
+			return 2;
+		}
+	}
+}
---
+++ src/main/java/com/the0show/randommod/procedures/FireStickLivingEntityIsHitWithToolProcedure.java
@@ -0,0 +1,23 @@
+package com.the0show.randommod.procedures;
+
+import net.minecraft.entity.Entity;
+
+import java.util.Map;
+
+import com.the0show.randommod.RandomModModElements;
+
+@RandomModModElements.ModElement.Tag
+public class FireStickLivingEntityIsHitWithToolProcedure extends RandomModModElements.ModElement {
+	public FireStickLivingEntityIsHitWithToolProcedure(RandomModModElements instance) {
+		super(instance, 3);
+	}
+
+	public static void executeProcedure(Map<String, Object> dependencies) {
+		if (dependencies.get("entity") == null) {
+			System.err.println("Failed to load dependency entity for procedure FireStickLivingEntityIsHitWithTool!");
+			return;
+		}
+		Entity entity = (Entity) dependencies.get("entity");
+		entity.setFire((int) 5);
+	}
+}
