1fc8fbc420bd2da65ad4ad0487e0ff52e73b733a
The0Show <jomst@DESKTOP-9RF5ELP>
2020-09-16 16:59:11 -0400

    added the creative tab

---
+++ src/main/java/com/the0show/randommod/itemgroup/RandomModTabItemGroup.java
@@ -0,0 +1,35 @@
+
+package com.the0show.randommod.itemgroup;
+
+import net.minecraftforge.api.distmarker.OnlyIn;
+import net.minecraftforge.api.distmarker.Dist;
+
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.block.Blocks;
+
+import com.the0show.randommod.RandomModModElements;
+
+@RandomModModElements.ModElement.Tag
+public class RandomModTabItemGroup extends RandomModModElements.ModElement {
+	public RandomModTabItemGroup(RandomModModElements instance) {
+		super(instance, 1);
+	}
+
+	@Override
+	public void initElements() {
+		tab = new ItemGroup("tabrandom_mod_tab") {
+			@OnlyIn(Dist.CLIENT)
+			@Override
+			public ItemStack createIcon() {
+				return new ItemStack(Blocks.OAK_LOG, (int) (1));
+			}
+
+			@OnlyIn(Dist.CLIENT)
+			public boolean hasSearchBar() {
+				return true;
+			}
+		}.setBackgroundImageName("item_search.png");
+	}
+	public static ItemGroup tab;
+}
