c8184953a33e9a07f7184feda61e87abad040cc3
The0Show <jomst@DESKTOP-9RF5ELP>
2020-09-16 23:22:20 -0400

    Added a new dimention :o

---
+++ src/main/java/com/the0show/randommod/item/DiamondDimentionItem.java
@@ -0,0 +1,42 @@
+
+package com.the0show.randommod.item;
+
+import net.minecraftforge.registries.ObjectHolder;
+
+import net.minecraft.world.World;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.ActionResultType;
+import net.minecraft.item.ItemUseContext;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Item;
+import net.minecraft.entity.player.PlayerEntity;
+
+import com.the0show.randommod.world.dimension.DiamondDimentionDimension;
+import com.the0show.randommod.itemgroup.RandomModTabItemGroup;
+
+public class DiamondDimentionItem extends Item {
+	@ObjectHolder("random_mod:diamond_dimention")
+	public static final Item block = null;
+	public DiamondDimentionItem() {
+		super(new Item.Properties().group(RandomModTabItemGroup.tab).maxDamage(64));
+	}
+
+	@Override
+	public ActionResultType onItemUse(ItemUseContext context) {
+		PlayerEntity entity = context.getPlayer();
+		BlockPos pos = context.getPos().offset(context.getFace());
+		ItemStack itemstack = context.getItem();
+		World world = context.getWorld();
+		if (!entity.canPlayerEdit(pos, context.getFace(), itemstack)) {
+			return ActionResultType.FAIL;
+		} else {
+			int x = pos.getX();
+			int y = pos.getY();
+			int z = pos.getZ();
+			if (world.isAirBlock(pos) && true)
+				DiamondDimentionDimension.portal.portalSpawn(world, pos);
+			itemstack.damageItem(1, entity, c -> c.sendBreakAnimation(context.getHand()));
+			return ActionResultType.SUCCESS;
+		}
+	}
+}
